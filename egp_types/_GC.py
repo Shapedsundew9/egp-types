d4a650f017aeb18e46c5447e3a17226cf51c8fba
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2023-01-21 10:27:21 -0500

    Notes on *GC structure and purpose.

--- egp_types/_GC.py
+++ egp_types/_GC.py
@@ -1,29 +1,42 @@
+"""_GC is the abstract base class for *GC classes.
+
+*GC classes are dict-like classes. Whilst not actually a Typeddict, validation
+restricts the fields that are present in each. Some fields are optional and/or
+derived.
+*GC classes stack with the following hierarchy:
+# _GC   Common abstract base class
+# eGC   Embryonic GC - may be unstable
+# mGC   Minimum viable GC - must be stable
+# gGC   Gene Pool Cache (GPC) GC - all fields required to be defined for the GPC
+# xGC   Extended GC - includes transient fields
+Each *GC is a superset of the one below.
+The goal of the class is performance and storage efficiency. eGC, mGC & gGC derived
+from the standard dict class for access performance and flexibility during live
+computation. The xGC class which has a highly optimised in memory storage
+implementation.
+"""
 from logging import DEBUG, NullHandler, getLogger, Logger
 from .xgc_validator import xgc_validator_generator, XGC_ENTRY_SCHEMA
 from .reference import ref_str
+
 
 _logger: Logger = getLogger(__name__)
 _logger.addHandler(NullHandler())
 _LOG_DEBUG: bool = _logger.isEnabledFor(DEBUG)
 
 
-class _GC(dict):
-    """Abstract base class for genetic code (GC) types.
-
-    Validity is in the context of a steady state GC i.e. an INVALID field will not cause
-    a validate() failure but the GC will not be stable (valid).
+class _GC():
+    """Abstract base class for genetic code (*GC) types.
 
     All derived classes mutate the the supplied dict-like object to make
     it a valid _GC type. The dict is NOT copied.
     """
 
-    _GC_ENTRIES: tuple[str, ...] = ('ref',)
-    validator: xgc_validator_generator = xgc_validator_generator({k: XGC_ENTRY_SCHEMA[k] for k in _GC_ENTRIES}, allow_unknow=True)
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    _GC_ENTRIES: None
+    validator: None
 
     def __repr__(self) -> str:
+        """Pretty print."""
         retval: str = '\t{\n'
         for k, v in sorted(self.items(), key=lambda x: x[0]):
             retval = retval + '\t\t' + f"'{k}'{' ' * (21 - len(k))}: "
@@ -35,6 +48,10 @@ class _GC(dict):
         return retval + '\t}'
 
     def validate(self) -> None:
-        """Validate all required key:value pairs are correct."""
+        """Validate all required key:value pairs are correct.
+        
+        Validation is an expensive operation and should only be done for debugging.
+        Where possible values are checked for consistency.
+        """
         if not self.validator.validate(self):
             raise ValueError(f"Validation FAILED with:\n{self.validator.error_str()}")
